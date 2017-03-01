31df1ed3f74c0f02611e68f3fa924e6c466c4e48
Adrien "Pandark" Pachkoff <adrien-dev@pachkoff.com>
2017-03-01 04:36:06 +0100

    update admin accounts

--- admins.js
+++ admins.js
@@ -1,10 +1,34 @@
 var admins = [
-  {id: 'U0GQXP468', overlord: true,  name: 'pk'},
-  {id: 'U0FSDKUSZ', overlord: false, name: 'dbourdon'},
-  {id: 'U2JEH63RB', overlord: false, name: 'lbenamer'},
-  {id: 'U0BS3SQAC', overlord: false, name: 'tet'},
-  {id: 'U047JPZG0', overlord: false, name: 'sle-guil'},
-  {id: 'U3T4Q0J0P', overlord: false, name: 'el_famoso_sneaky'}
+  {
+    "id": "U0GQXP468",
+    "overlord": true,
+    "name": "pk"
+  },
+  {
+    "id": "U0FSDKUSZ",
+    "overlord": false,
+    "name": "dbourdon"
+  },
+  {
+    "id": "U2JEH63RB",
+    "overlord": false,
+    "name": "lbenamer"
+  },
+  {
+    "id": "U0BS3SQAC",
+    "overlord": false,
+    "name": "tet"
+  },
+  {
+    "id": "U047JPZG0",
+    "overlord": false,
+    "name": "sle-guil"
+  },
+  {
+    "id": "U459DCQLF",
+    "overloard": false,
+    "name": "kurgerbing_2"
+  }
 ]
 
 module.exports = admins
